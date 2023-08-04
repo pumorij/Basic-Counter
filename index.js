0de5f8f0d5a8866c49e8489ba4b3a2e2bf5d81e9
pumorij <pumori2015108@gndec.ac.in>
2023-08-04 10:19:30 +0530

    comments added

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const countVal = document.querySelector('#counter')
 
 const increment=()=>{
+    // get value
     let value = parseInt(countVal.innerText);
+    // increment value
     value = value+1
+    // put value
     countVal.innerText=value
 }
 
 const decrement=()=>{
+    // get value
     let value = parseInt(countVal.innerText);
+    // decrement value
     value = value-1
+    // put value
     countVal.innerText=value
 }
